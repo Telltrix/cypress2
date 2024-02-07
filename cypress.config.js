51ff96f9cd824840d5fe12b477ef1be56e2be109
Telltrix <telltrix@hotmail.com>
2024-02-06 21:08:36 -0300

    sera

--- cypress.config.js
+++ cypress.config.js
@@ -2,11 +2,12 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
-    // We've imported your old cypress plugins here.
-    // You may want to clean this up later by importing these.
+    viewportHeight: 880,
+    viewportWidth: 1280,
     setupNodeEvents(on, config) {
       return require('./cypress/plugins/index.js')(on, config)
     },
+    experimentalWebKitSupport: true,
     baseUrl: 'https://barrigarest.wcaquino.me',
   },
 })
